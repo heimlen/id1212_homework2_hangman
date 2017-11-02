54f46b42c1865714e605159b0a9f5e22897e850c
Sebastian Heimlén <sebastian@heimlen.se>
2017-11-02 13:11:58 +0100

    Created general file structure

---
+++ src/main/java/se/kth/id1212/ASDF.java
@@ -0,0 +1,7 @@
+package se.kth.id1212;
+
+/**
+ * Created by heimlen on 2017-11-02.
+ */
+public class ASDF {
+}
---
+++ src/main/java/se/kth/id1212/heimlen/homework1/startup/Main.java
@@ -0,0 +1,7 @@
+package se.kth.id1212.heimlen.homework1.startup;
+
+/**
+ * Created by heimlen on 2017-11-02.
+ */
+public class Main {
+}
